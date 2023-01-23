80458f205b5f8f3647ad4e8236c30ea6c2c20d62
Nabil Hadi <napilfadl@gmail.com>
2023-01-23 11:30:35 +0200

    Add helper dom function

--- script.js
+++ script.js
@@ -27,6 +27,45 @@ const PubSub = {
   },
 };
 
+function createElement({
+  tag = "div",
+  classNames = [],
+  textContent,
+  dataset = {},
+  attributes = {},
+  eventHandlers = {},
+} = {}) {
+  // Create elm with tag
+  const elm = document.createElement(tag);
+
+  // Add classes
+  classNames.forEach((className) => {
+    elm.classList.add(className);
+  });
+
+  // Set textContent
+  if (textContent) {
+    elm.textContent = textContent;
+  }
+
+  // Set dataset
+  for (const key in dataset) {
+    elm.dataset[key] = dataset[key];
+  }
+
+  // Set Attribuites
+  for (const key in attributes) {
+    elm.setAttribute(key, attributes[key]);
+  }
+
+  // Set Handlers
+  for (const key in eventHandlers) {
+    elm.addEventListener(key, eventHandlers[key]);
+  }
+
+  return elm;
+}
+
 const Library = (function () {
   function Book(title, author, pages, isRead = false) {
     this.title = title;
