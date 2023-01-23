9552ee6a0bc36623828a00ab3d035b37fcaf4297
Nabil Hadi <napilfadl@gmail.com>
2023-01-23 13:13:48 +0200

    Add modal logic and styles

--- script.js
+++ script.js
@@ -66,6 +66,66 @@ function createElement({
   return elm;
 }
 
+const modal = (function () {
+  const view = createElement({
+    tag: "div",
+    classNames: ["modal"],
+    attributes: { id: "modal" },
+  });
+
+  const modalContent = createElement({
+    tag: "div",
+    classNames: ["modal-content"],
+    attributes: {
+      id: "modal-content",
+    },
+  });
+  view.append(modalContent);
+
+  const closeBtn = createElement({
+    tag: "span",
+    classNames: ["close"],
+    textContent: "×",
+  });
+  modalContent.append(closeBtn);
+
+  closeBtn.addEventListener("click", () => {
+    hideModal();
+  });
+
+  view.addEventListener(
+    "click",
+    (e) => {
+      if (view !== e.target) return;
+      hideModal();
+    },
+    false
+  );
+
+  document.querySelector(".container").appendChild(view);
+
+  function setContent(content = []) {
+    modalContent.innerHTML = "";
+    modalContent.append(closeBtn);
+
+    modalContent.append(...content);
+  }
+
+  function showModal() {
+    view.classList.add("show");
+  }
+
+  function hideModal() {
+    view.classList.remove("show");
+  }
+
+  function getView() {
+    return view;
+  }
+
+  return { setContent, showModal, hideModal, getView };
+})();
+
 const Library = (function () {
   function Book(title, author, pages, isRead = false) {
     this.title = title;
