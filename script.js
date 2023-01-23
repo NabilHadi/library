7b50ee6bb56a16a3596cb1b839af4e27c29835b3
Nabil Hadi <napilfadl@gmail.com>
2023-01-23 11:27:36 +0200

    Add modules to code

--- script.js
+++ script.js
@@ -27,61 +27,71 @@ const PubSub = {
   },
 };
 
-let myLibrary = [
-  new Book("The Hobbit", "J.R.R.", 295, false),
-  new Book("The not Hobbit", "not J.R.R.", 195, true),
-  new Book("Hello", "World", 15, true),
-];
-
-function Book(title, author, pages, isRead = false) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.isRead = isRead;
-}
-
-function addBookToLibrary(bookObj) {
-  myLibrary.push(
-    new Book(bookObj.title, bookObj.author, bookObj.pages, bookObj.isRead)
-  );
-
-  PubSub.publish("BookAdded", myLibrary);
-}
-
-function displayBooks(books) {
-  const bookUL = document.querySelector(".books-list");
-  bookUL.innerHTML = "";
-  const booksLis = books.map((book) => {
-    const li = document.createElement("li");
-    li.textContent = `${book.title} by ${book.author}, ${book.pages} pages, ${
-      book.isRead ? "Read" : "Not read"
-    }.`;
-    return li;
-  });
-
-  bookUL.append(...booksLis);
-}
-
-function handleFormSubmit(e) {
-  e.preventDefault();
-
-  const formData = new FormData(e.target);
-  let bookObj = {};
-  for (const [name, value] of formData) {
-    if (name === "read") {
-      bookObj.isRead = value === "on" ? true : false;
-      continue;
-    }
-    bookObj[name] = value;
+const Library = (function () {
+  function Book(title, author, pages, isRead = false) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
   }
 
-  PubSub.publish("addBookToLibrary", bookObj);
-}
+  let books = [
+    new Book("The Hobbit", "J.R.R.", 295, false),
+    new Book("The not Hobbit", "not J.R.R.", 195, true),
+    new Book("Hello", "World", 15, true),
+  ];
 
-const newBookForm = document.querySelector("#new_book_form");
-newBookForm.addEventListener("submit", handleFormSubmit);
+  return {
+    addBookToLibrary(bookObj) {
+      books.push(
+        new Book(bookObj.title, bookObj.author, bookObj.pages, bookObj.isRead)
+      );
 
-PubSub.subscribe("addBookToLibrary", addBookToLibrary);
-PubSub.subscribe("BookAdded", displayBooks);
+      PubSub.publish("BookAdded", books);
+    },
+    getBooks() {
+      return [...books];
+    },
+  };
+})();
 
-displayBooks(myLibrary);
+const BookForm = (function () {
+  function handleFormSubmit(e) {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    let bookObj = {};
+    for (const [name, value] of formData) {
+      if (name === "read") {
+        bookObj.isRead = value === "on" ? true : false;
+        continue;
+      }
+      bookObj[name] = value;
+    }
+
+    PubSub.publish("addBookToLibrary", bookObj);
+  }
+  const newBookForm = document.querySelector("#new_book_form");
+  newBookForm.addEventListener("submit", handleFormSubmit);
+})();
+
+const BookList = (function () {
+  function displayBooks(books) {
+    const bookUL = document.querySelector(".books-list");
+    bookUL.innerHTML = "";
+    const booksLis = books.map((book) => {
+      const li = document.createElement("li");
+      li.textContent = `${book.title} by ${book.author}, ${book.pages} pages, ${
+        book.isRead ? "Read" : "Not read"
+      }.`;
+      return li;
+    });
+
+    bookUL.append(...booksLis);
+  }
+
+  PubSub.subscribe("BookAdded", displayBooks);
+  displayBooks(Library.getBooks());
+})();
+
+PubSub.subscribe("addBookToLibrary", Library.addBookToLibrary);
