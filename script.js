69d794dfac79d9d49ae7b9168718588e8d620c87
Nabil Hadi <42745851+NabilHadi@users.noreply.github.com>
2024-05-03 15:27:05 +0300

    Add book row componenet

--- script.js
+++ script.js
@@ -1,98 +1,122 @@
 const library = (function () {
-  const books = [];
 
+  // Book constructor
   function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-
-    this.info = function () {
-      return `${title} by ${author}, ${pages} pages, ${read ? "read" : "not read yet"}`;
-    };
   }
+
+  Book.prototype.info = function () {
+    return `${title} by ${author}, ${pages} pages, ${read ? "read" : "not read yet"}`;
+  };
 
   Book.prototype.toggleRead = function () {
     this.read = !this.read;
   };
 
-  books.push(new Book("my title", "my Author", 123, false),
-    new Book("my title 1", "my Author", 135, true),
-    new Book("my title 2", "my Author 1", 153, false),
-    new Book("my title 3", "my Author 2", 423, true),);
+  let currentId = 0;
+  const books = [];
+  // Book row component
+  function createBookRow({ title, author, pages, read }, onBookDelete, onReadToggle) {
+    const id = currentId++;
+    const book = new Book(title, author, pages, read);
+
+    const tableRow = document.createElement("tr");
+    const tdTitle = document.createElement("td");
+    tdTitle.textContent = book.title;
+    tableRow.appendChild(tdTitle);
+
+    const tdAuthor = document.createElement("td");
+    tdAuthor.textContent = book.author;
+    tableRow.appendChild(tdAuthor);
+
+    const tdPages = document.createElement("td");
+    tdPages.textContent = book.pages;
+    tableRow.appendChild(tdPages);
+
+    const tdRead = document.createElement("td");
+    tdRead.textContent = book.read ? "Read" : "Not read yet";
+    tableRow.appendChild(tdRead);
+
+    const tdEdit = document.createElement("td");
+    const div = document.createElement("div");
+    div.classList.add("book_edit_container");
+    const deleteBookBtn = document.createElement("button");
+    deleteBookBtn.classList.add("btn");
+    deleteBookBtn.textContent = "REMOVE";
+
+    function deleteBook() {
+      deleteBookBtn.removeEventListener("click", deleteBook);
+      toggleReadBtn.removeEventListener("click", toggleRead);
+      tableRow.remove();
+      onBookDelete && onBookDelete(id);
+    }
+
+    deleteBookBtn.addEventListener("click", deleteBook);
+
+    const toggleReadBtn = document.createElement("button");
+    toggleReadBtn.classList.add("btn");
+    toggleReadBtn.textContent = book.read ? "Unread" : "read";
+
+    function toggleRead() {
+      book.toggleRead();
+      tdRead.textContent = book.read ? "Read" : "Not read yet";
+      onReadToggle && onReadToggle();
+    }
+
+    toggleReadBtn.addEventListener("click", toggleRead);
 
 
-  function addBookToLibrary(title, author, pages, read) {
-    books.push(new Book(title, author, pages, read));
+    div.appendChild(toggleReadBtn);
+    div.appendChild(deleteBookBtn);
+    tdEdit.appendChild(div);
+    tableRow.appendChild(tdEdit);
+
+
+    return {
+      id,
+      row: tableRow,
+      deleteBook,
+      toggleRead,
+    };
+  }
+
+  function onBookDelete(id) {
+    const i = books.findIndex(book => book.id === id);
+    if (i < 0) return;
+    books.splice(books.indexOf(i), 1);
     render();
   }
 
-
+  function addBookToLibrary(...newBooks) {
+    newBooks.forEach(({ title, author, pages, read }) => {
+      books.push(createBookRow({ title, author, pages, read }, onBookDelete));
+    });
+    render();
+  }
 
   const table = document.querySelector("#books-table");
   const tableBody = table.querySelector("tbody");
 
   function render() {
-    // Clear table body
-    let child = tableBody.lastElementChild;
-    while (child) {
-      tableBody.removeChild(child);
-      child = tableBody.lastElementChild;
-    }
-
     books.forEach(book => {
-      const tableRow = document.createElement("tr");
-      const tdTitle = document.createElement("td");
-      tdTitle.textContent = book.title;
-      tableRow.appendChild(tdTitle);
-
-      const tdAuthor = document.createElement("td");
-      tdAuthor.textContent = book.author;
-      tableRow.appendChild(tdAuthor);
-
-      const tdPages = document.createElement("td");
-      tdPages.textContent = book.pages;
-      tableRow.appendChild(tdPages);
-
-      const tdRead = document.createElement("td");
-      tdRead.textContent = book.read ? "Read" : "Not read yet";
-      tableRow.appendChild(tdRead);
-
-      const tdEdit = document.createElement("td");
-      const div = document.createElement("div");
-      div.classList.add("book_edit_container");
-      const deleteBookBtn = document.createElement("button");
-      deleteBookBtn.classList.add("btn");
-      deleteBookBtn.textContent = "REMOVE";
-      deleteBookBtn.addEventListener("click", () => {
-        books.splice(books.indexOf(book), 1);
-        render();
-      });
-
-      const toggleReadBtn = document.createElement("button");
-      toggleReadBtn.classList.add("btn");
-      toggleReadBtn.textContent = book.read ? "Unread" : "read";
-      toggleReadBtn.addEventListener("click", () => {
-        book.toggleRead();
-        render();
-      });
-
-
-      div.appendChild(toggleReadBtn);
-      div.appendChild(deleteBookBtn);
-      tdEdit.appendChild(div);
-      tableRow.appendChild(tdEdit);
-
-      tableBody.appendChild(tableRow);
+      tableBody.appendChild(book.row);
     });
   }
 
-  render();
+  // Add books
+  addBookToLibrary(
+    { title: "my title", author: "my Author", pages: 123, read: false },
+    { title: "my title 1", author: "my Author", pages: 135, read: true },
+    { title: "my title 2", author: "my Author 1", pages: 153, read: false },
+    { title: "my title 3", author: "my Author 2", pages: 423, read: true });
 
 
   return {
     getBooks() {
-      return books;
+      return [...books];
     },
     addBookToLibrary
   };
@@ -121,8 +145,7 @@ const bookForm = (function () {
     const pages = document.querySelector("#pages_input").value;
     const read = document.querySelector("#read_input").checked;
 
-    console.log({ author, title, pages, read });
-    library.addBookToLibrary(title, author, Number(pages), read);
+    library.addBookToLibrary({ title, author, pages: Number(pages), read });
 
     form.reset();
     formDialog.close();
